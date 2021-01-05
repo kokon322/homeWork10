aa8fee014be951beff49648b8619de1d294d2a71
kokon322 <chornuy322@gmail.com>
2021-01-05 12:47:34 +0100

    UserDaoImpl is done

--- src/main/java/dao/impl/UserDaoImpl.java
+++ src/main/java/dao/impl/UserDaoImpl.java
@@ -2,17 +2,51 @@ package dao.impl;
 
 import dao.UserDao;
 import model.User;
+import util.ConnectionUtil;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoImpl implements UserDao {
+
+
     @Override
     public User save(User user) {
-        return null;
+        String query = "INSERT INTO users (user_name, user_age) VALUES (?, ?)";
+        try (PreparedStatement preparedStatement = ConnectionUtil.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, user.getUserName());
+            preparedStatement.setInt(2, user.getAge());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getLong(1));
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return user;
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        List<User> users = new ArrayList<>();
+        String query = "SELECT * FROM users";
+        try (PreparedStatement statement = ConnectionUtil.getConnection().prepareStatement(query)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setUserName(resultSet.getString("user_name"));
+                user.setAge(resultSet.getInt("user_age"));
+                users.add(user);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return users;
     }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,4 +4,28 @@ public class User {
     private Long id;
     private String userName;
     private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
